b9a00bf96ce371c0e42a37eb67921f41702a3443
Hitchnt <56658191+Hitchnt@users.noreply.github.com>
2021-09-18 19:46:35 -0400

    Changes by JDMADD

--- src/main/java/com/enterprise/redcord/RedCordController.java
+++ src/main/java/com/enterprise/redcord/RedCordController.java
@@ -1,5 +1,10 @@
 package com.enterprise.redcord;
 
+import com.enterprise.redcord.dto.Message;
+import com.enterprise.redcord.dto.Topic;
+import com.enterprise.redcord.service.IMessageService;
+import com.enterprise.redcord.service.ITopicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RedCordController {
 
+    @Autowired
+    ITopicService topicService;
+    @Autowired
+    IMessageService messageService;
+
     /**
      * Handle the root (/) endpoint and return a start page
      * @return
      */
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        Message message = new Message();
+        model.addAttribute(message);
         return "start";
     }
+    
+    
+    
+    @RequestMapping("/saveTopic")
+    public String index(Topic topic) {
+       try {
+           topicService.save(topic);
+       }catch(Exception e){
+           e.printStackTrace();
+        }
+        return "start";
+    }
+
+
+
+
+
 }
