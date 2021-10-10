ee9f67ee4888a48610ce73c902c71265dc39a44d
Bipal Goyal <goyalbl@mail.uc.edu>
2021-10-11 02:36:58 +0545

    Added logger to RedCordController

--- src/main/java/com/enterprise/redcord/RedCordController.java
+++ src/main/java/com/enterprise/redcord/RedCordController.java
@@ -4,6 +4,8 @@ import com.enterprise.redcord.dto.Message;
 import com.enterprise.redcord.dto.Topic;
 import com.enterprise.redcord.service.IMessageService;
 import com.enterprise.redcord.service.ITopicService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Controller
 public class RedCordController {
+
+    Logger logger = LoggerFactory.getLogger(RedCordController.class);
 
     @Autowired
     ITopicService topicService;
@@ -26,17 +30,22 @@ public class RedCordController {
     @RequestMapping("/")
     public String index(Model model) {
         Message messageEntry = new Message();
+
+        logger.trace("Accessed index method in RedCordController.");
+
         model.addAttribute(messageEntry);
         return "start";
     }
-    
-    
+
     
     @RequestMapping("/saveTopic")
     public String saveTopic(Topic topic) {
+       logger.trace("Accessed savedTopic method in RedCordController.");
+
        try {
            topicService.save(topic);
        } catch(Exception e) {
+           logger.error("Error in saveTopic method: " + e.getMessage());
            e.printStackTrace();
        }
        return "start";
@@ -44,9 +53,12 @@ public class RedCordController {
 
     @RequestMapping("/saveMessage")
     public String saveMessage(Message messageEntry) {
+        logger.trace("Accessed saveMessage method in RedCordController.");
+
         try {
             messageService.saveMessage(messageEntry);
         } catch(Exception e) {
+            logger.error("Error in saveMessage method: " + e.getMessage());
             e.printStackTrace();
         }
         return "start";
@@ -55,11 +67,12 @@ public class RedCordController {
     @GetMapping("/allMessages")
     @ResponseBody
     public List<Message> fetchAllMessages() {
-
         logger.trace("Accessed fetchAllMessages method in RedCordController.");
+
         try {
             return messageService.fetchAllMessages();
         } catch (Exception e) {
+            logger.error("Error in fetchAllMessages method: " + e.getMessage());
             e.printStackTrace();
             return null;
         }
