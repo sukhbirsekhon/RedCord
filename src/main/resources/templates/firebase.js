1762a2c584de03d1d1e072b37613f14cd0fc2028
Rohan Patel <patel4r4@mail.uc.edu>
2021-11-07 15:52:11 -0500

    added login page
    userface for login was added

---
+++ src/main/resources/templates/firebase.js
@@ -0,0 +1,18 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWfT2wgiqgNmbHXfCnkYXNg-kT7ny0a1I",
+    authDomain: "redcord-cad62.firebaseapp.com",
+    databaseURL: "https://redcord-cad62-default-rtdb.firebaseio.com",
+    projectId: "redcord-cad62",
+    storageBucket: "redcord-cad62.appspot.com",
+    messagingSenderId: "572988694939",
+    appId: "1:572988694939:web:078360c7afac6c5687813e",
+    measurementId: "G-HQ3YJ8S3ZS"
+};
+
+function initializeApp(firebaseConfig) {
+    return undefined;
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
